911edbca81cf0cc4d14caf6439091e605a3a1e22
Aizirek Bakirova <aizirek.bakirova@gmail.com>
2024-05-06 18:45:50 +0600

    initial commit

---
+++ lib/internal/application.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:sunrise_sunset_app/presentation/home.dart';
+
+class Application extends StatelessWidget {
+  const Application({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity),
+      home: Home(),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:sunrise_sunset_app/internal/application.dart';
+
+void main() {
+  runApp(const Application());
+}
